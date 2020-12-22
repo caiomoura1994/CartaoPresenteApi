805667cbb26c3f24a4ed6da95bb0f028d82c1bdf
Caio Barros de Moura <caiomoura1994@gmail.com>
2020-12-22 02:13:06 -0300

    add related Images in serializers.py

--- contents/serializers.py
+++ contents/serializers.py
@@ -1,13 +1,26 @@
 from rest_framework import serializers
 
-from .models import Product, Order
+from .models import Product, Order, Image
+
+
+class ImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Image
+        fields = [
+            'original',
+            'medium',
+            'small',
+        ]
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True)
 
     class Meta:
         model = Product
         fields = '__all__'
+
 
 class OrderSerializer(serializers.ModelSerializer):
 
